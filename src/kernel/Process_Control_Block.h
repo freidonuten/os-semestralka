debd112a55bbb6bf827bfc479f2006787f60b463
Martin Procházka <martin.prochazka@protonmail.com>
2021-10-24 12:53:12 -0700

    Updated TCB and PCB headers - still not final but should allow for some basic functionality like forking a process/creating threads

--- src/kernel/Process_Control_Block.h
+++ src/kernel/Process_Control_Block.h
@@ -1,22 +1,41 @@
 #pragma once
 
+#include <unordered_set>
+
+
+enum class Process_State : uint8_t {
+	FREE = 0, // Process slot is available for allocation
+	READY,
+	RUNNING,
+	FINISHED,
+};
+
 class Process_Control_Block final {
 private:
-	using Thread_Pool = std::array<kiv_os::THandle, constants::thread_per_process>;
-	using FD_Pool = std::array<kiv_os::THandle, constants::fd_per_process>;
+	using Thread_Pool = std::unordered_set<kiv_os::THandle>;
+	using FD_Pool = std::unordered_set<kiv_os::THandle>;
+	using Buffer = char[64]; // get rid of the magic number
 
-	const kiv_os::THandle PID;
-	Thread_Pool thread_pool;
-	FD_Pool fd_pool;
+	uint32_t pid;
+	uint32_t tid;
+	Process_State state = Process_State::FREE;
+	Thread_Pool thread_list;
+	FD_Pool fd_list;
+	Buffer cwd = "/";
+	
 
 public:
-	explicit Process_Control_Block() = delete;
+	explicit Process_Control_Block();
 	explicit Process_Control_Block(const kiv_os::THandle pid);
 
 	const kiv_os::THandle get_pid() const;
+	const Process_State get_state() const;
+	const char* get_cwd() const;
 	
 	void thread_insert(const kiv_os::THandle tid);
 	void thread_remove(const kiv_os::THandle tid);
 	void fd_insert(const kiv_os::THandle fd);
 	void fd_remove(const kiv_os::THandle fd);
+	void set_cwd(const char* buffer);
+	void allocate();
 };
--- src/kernel/Thread_Control_Block.h
+++ src/kernel/Thread_Control_Block.h
@@ -1,17 +1,35 @@
 #pragma once
 
+#include <thread>
+#include "../api/api.h"
+#include "Process_Control_Block.h"
+
+
+enum class Thread_State {
+	FREE = 0,
+	BLOCKED,
+	READY,
+	RUNNING
+};
+
 class Thread_Control_Block final {
 private:
-	const kiv_os::THandle TID;	// thread id
-	const kiv_os::THandle PPID; // parent process id
+	kiv_os::THandle ppid; // parent process id
+	uint32_t return_code;
+	kiv_hal::TRegisters context;
+	std::thread instance;
+	Thread_State state = Thread_State::FREE;
+	char** args; // null terminated strings?
 
-	char state; // TODO: make it enum
 
 public:
-	// thread always needs a parent
-	explicit Thread_Control_Block() = delete;
-	explicit Thread_Control_Block(const kiv_os::THandle ppid);
+
+	kiv_os::THandle get_tid() const;
+	kiv_os::THandle get_ppid() const;
+	Thread_State get_state() const;
+	bool is_current() const;
 
 	// TODO: state manipulation
-	
+	void allocate(const kiv_os::TThread_Proc& entry, const kiv_hal::TRegisters& context);
+	void adopt(Process_Control_Block& parent);
 };
