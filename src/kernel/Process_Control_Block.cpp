f619dad22fc56a45b83b189213607f6bb440c82d
Martin Procházka <martin.prochazka@protonmail.com>
2021-10-28 02:31:11 -0700

    Implemented getters in PCB

--- src/kernel/Process_Control_Block.cpp
+++ src/kernel/Process_Control_Block.cpp
@@ -1,6 +1,21 @@
 #include "Process_Control_Block.h"
 
 
+const kiv_os::THandle Process_Control_Block::get_pid() const
+{
+	return pid;
+}
+
+const Execution_State Process_Control_Block::get_state() const
+{
+	return state;
+}
+
+const char* Process_Control_Block::get_cwd() const
+{
+	return cwd;
+}
+
 const bool Process_Control_Block::is_main_thread(const kiv_os::THandle tid) const {
 	return this->tid == tid;
 }
