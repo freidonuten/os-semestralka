402c819050825fb52e3c567e161c10970a3e2b75
Martin Procházka <martin.prochazka@protonmail.com>
2021-10-28 02:32:18 -0700

    Implemented PCB ctors

--- src/kernel/Process_Control_Block.cpp
+++ src/kernel/Process_Control_Block.cpp
@@ -1,6 +1,13 @@
 #include "Process_Control_Block.h"
 
 
+Process_Control_Block::Process_Control_Block()
+{ }
+
+Process_Control_Block::Process_Control_Block(const kiv_os::THandle pid)
+	: pid(pid)
+{ }
+
 const kiv_os::THandle Process_Control_Block::get_pid() const
 {
 	return pid;
