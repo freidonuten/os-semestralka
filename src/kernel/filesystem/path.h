9b888e4239d997d2e1c740d4a50c86221db4b5a5
Štěpán Červenka <stepan.cervenka1@gmail.com>
2021-11-06 11:46:58 +0100

    Revert "Path Encapsulation"
    This reverts commit 1e18424869973bd269f72f82d5ca0e01361d1346.

--- src/kernel/filesystem/path.h
+++ src/kernel/filesystem/path.h
@@ -5,33 +5,11 @@
 
 #include <memory>
 
-constexpr auto MAX_FILENAME_SIZE = 12;
-constexpr auto MAX_PATH_SIZE = 256;
-
-class Filename {
-private:
-	std::vector<char> implementation;
-public:
-	void Add_Char(char character);
-	void Copy_To_Array(char* result, int count);
-	int Get_Length();
-};
-
 class Path {
 private:
-	std::vector<std::shared_ptr<Filename>> filenames;
-	int Append_Buffer(char* buffer, int buffer_index, std::shared_ptr<Filename> filename);
+	static std::vector<std::array<char, 12>> Parse_Path_To_Filenames(char* path);
+	static void Copy_To_Result(char* dest, int dest_i, char* source, int source_i);
+	static void Add_To_Vector(std::vector<std::array<char, 12>> result, char current_filename[12]);
 public:
-	Path(char* path);
-
-	int Get_Path_Length();
-	void Get_Path(char* buffer);
-	void Get_Path(char* buffer, int filename_count);
-
-	std::shared_ptr<VFS_Element> Get_Element(std::shared_ptr<VFS_Element> from);
-	void Append(char* path);
-	std::shared_ptr<Path> Get_Parent();
-
+	static Element_And_Parent Get_Element_From_Path(std::shared_ptr<VFS_Element> directory, char* path);
 };
-
-
