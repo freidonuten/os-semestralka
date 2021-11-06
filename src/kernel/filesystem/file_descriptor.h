b725f366e1bc0fd042e42177e67120f66b8434ef
Štěpán Červenka <stepan.cervenka1@gmail.com>
2021-11-06 11:49:19 +0100

    Revert "File Descriptors Redesign"
    This reverts commit 4648ee0ec6a3116b91a710c0fc2430ba0a456f98.

--- src/kernel/filesystem/file_descriptor.h
+++ src/kernel/filesystem/file_descriptor.h
@@ -4,16 +4,23 @@
 #include <memory>
 #include <vector>
 
-#include "vfs_element.h"
+struct File_Descriptor {
+	std::uint16_t descriptor_id;
+	std::uint16_t fat_first_cluster;
+	std::uint64_t file_size;
+	std::uint64_t position;
+};
 
 class File_Descriptor_Table {
 private:
-	std::vector<std::shared_ptr<VFS_Element>> descriptors;
-	bool Is_Valid(std::uint16_t id);
+	std::vector<std::shared_ptr<File_Descriptor>> descriptors;
+	void Add_Descriptor(std::shared_ptr<File_Descriptor> descriptor);
 public:
-	std::uint16_t Create_Descriptor(std::shared_ptr<VFS_Element> element);
-	std::shared_ptr<VFS_Element> Get_Descriptor(std::uint16_t id);
-	void Remove_Descriptor(std::uint16_t id);
+	File_Descriptor_Table();
+
+	std::shared_ptr<File_Descriptor> Create_Descriptor(std::uint16_t fat_first_cluster);
+	std::shared_ptr<File_Descriptor> Get_Descriptor(std::uint16_t id);
+	void Remove_Handle(std::uint16_t id);
 
 
 };
