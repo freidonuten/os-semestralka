af6ecef4fafddcb4db76da96c11fdcd56fc0485e
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-10 10:27:09 -0800

    Throw if not implemented
    Kinda dirty way to do this but still cleaner (imho) than forcing all derived classes to implement multiple methods that they should not need to implement in first place. I.e. What would be the expected behaviour of calling Pipe.Contains_Child()..?

--- src/kernel/filesystem/vfs_layer/vfs_element.cpp
+++ src/kernel/filesystem/vfs_layer/vfs_element.cpp
@@ -4,6 +4,7 @@
 #include "../utils/bit_utils.h"
 
 #include "vfs_file.h"
+#include <stdexcept>
 
 VFS_Element_Factory::VFS_Element_Factory(std::shared_ptr<IDisk> disk, std::shared_ptr<Filesystem_Info> info) {
 	this->file_factory = std::make_shared<Fat_File_Factory>(disk, info);
@@ -39,6 +40,30 @@ bool VFS_Element_Factory::Is_Directory(std::uint8_t file_attributes) {
 	return Bit_Utils::Check_Individual_Bit(file_attributes, 0x10);
 }
 
+void VFS_Element::Create() {
+	throw std::runtime_error("Unsupported operation CREATE.");
+}
+
+void VFS_Element::Open(std::uint16_t file_start, std::uint16_t file_size) {
+	throw std::runtime_error("Unsupported operation OPEN.");
+}
+
+bool VFS_Element::Remove() {
+	throw std::runtime_error("Unsupported operation OPEN.");
+}
+
+int VFS_Element::Write(std::uint64_t starting_byte, size_t how_many_bytes, void* buffer) {
+	throw std::runtime_error("Unsupported operation WRITE.");
+}
+
+int VFS_Element::Read(std::uint64_t starting_byte, size_t how_many_bytes, void* buffer) {
+	throw std::runtime_error("Unsupported operation READ.");
+}
+
+void VFS_Element::Change_Size(std::uint32_t desired_size) {
+	throw std::runtime_error("Unsupported operation CHANGE_SIZE.");
+}
+
 void VFS_Element::Change_Attributes(std::uint16_t file_attributes) {
 	this->file_attributes = file_attributes;
 	Fat_Dir_Entry entry = this->Generate_Dir_Entry();
@@ -51,6 +76,34 @@ std::uint16_t VFS_Element::Read_Attributes() {
 
 char* VFS_Element::Read_File_Name() {
 	return this->file_name;
+}
+
+std::shared_ptr<VFS_Element> VFS_Element::Create_Child(char file_name[12], std::uint16_t file_attributes) {
+	throw std::runtime_error("Unsupported operation CREATE_CHILD.");
+}
+
+std::shared_ptr<VFS_Element> VFS_Element::Open_Child(char file_name[12]) {
+	throw std::runtime_error("Unsupported operation OPEN_CHILD.");
+}
+
+bool VFS_Element::Contains_Child(char file_name[12]){
+	throw std::runtime_error("Unsupported operation READ.");
+}
+
+void VFS_Element::Remove_Child(char file_name[12]) {
+	throw std::runtime_error("Unsupported operation REMOVE_CHILD.");
+}
+
+std::shared_ptr<Fat_Dir_Entry> VFS_Element::Get_ChildO(char file_name[12]) {
+	throw std::runtime_error("Unsupported operation GET_CHILD_O.");
+}
+
+void VFS_Element::Update_ChildO(char old_file_name[12], Fat_Dir_Entry entry) {
+	throw std::runtime_error("Unsupported operation UPDATE_CHILD_O.");
+}
+
+Fat_Dir_Entry VFS_Element::Generate_Dir_Entry() {
+	throw std::runtime_error("Unsupported operation GENERATE_DIR_ENTRY.");
 }
 
 
