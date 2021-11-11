c59c52c55388ff925c022266af15c729c3493abd
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-11 06:09:24 -0800

    Fixed create_pipe_action
    Updated to use the new pipe factory

--- src/kernel/filesystem/actions/create_pipe_action.cpp
+++ src/kernel/filesystem/actions/create_pipe_action.cpp
@@ -2,12 +2,12 @@
 #include "../sysfs_layer/pipe.h"
 
 void Create_Pipe_Action::Run() {
-	auto write_end = std::make_shared<Pipe>();
-	auto read_end =  std::shared_ptr<Pipe>(write_end);
+	auto [write_end, read_end] = Pipe::Factory();
+
 	auto desc_table = vfs->Get_Descriptor_Table();
 	auto ptr = reinterpret_cast<kiv_os::THandle*>(this->registers->rdx.r);
 
 	// output is write end and read end of the pipe
-	ptr[0] = desc_table->Create_Descriptor(std::static_pointer_cast<VFS_Element>(write_end));
-	ptr[1] = desc_table->Create_Descriptor(std::static_pointer_cast<VFS_Element>(read_end));
+	ptr[0] = desc_table->Create_Descriptor(write_end);
+	ptr[1] = desc_table->Create_Descriptor(read_end);
 }
