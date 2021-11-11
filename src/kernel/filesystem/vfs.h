80f7b785c58ecf8d33c0bf9ec249d069592022e6
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-11 12:38:44 -0800

    VFS post-action-removal cleanup

--- src/kernel/filesystem/vfs.h
+++ src/kernel/filesystem/vfs.h
@@ -8,27 +8,18 @@
 #include "vfs_layer/file_descriptor.h"
 #include <memory>
 
-class Action_Factory;
-class Action;
 
-class VFS {
+class VFS final {
 private:
-	std::shared_ptr<Action_Factory> action_factory;
 	std::shared_ptr<VFS_Element_Factory> element_factory;
-
 	std::shared_ptr<VFS_Element> root;
 	std::shared_ptr<Path> current_path;
-	
 	std::shared_ptr<File_Descriptor_Table> descriptor_table;
-
 	
 public:
 	VFS();
 
-	void Proceed_Action(kiv_hal::TRegisters &regs);
-
 	std::shared_ptr<File_Descriptor_Table> Get_Descriptor_Table();
-
 	std::shared_ptr<VFS_Element> Get_Root();
 	std::shared_ptr<Path> Get_Path(char* path); //absolute or relative
 	std::shared_ptr<Path> Get_Current_Path();
