27570d96d3019eb1c69543608415f04132bac2bc
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-11 12:37:25 -0800

    Actions replaced with file_system::Dispatcher

--- src/kernel/filesystem/actions/action.h
+++ src/kernel/filesystem/actions/action.h
@@ -11,12 +11,26 @@
 #include <memory>
 
 
-class Action {
-protected:
-	kiv_hal::TRegisters *registers;
-	VFS *vfs;
-public:
-	Action(kiv_hal::TRegisters &registers, VFS *vfs);
-	virtual void Run() = 0;
-};
+namespace file_system {
+	class Dispatcher {
+	private:
+		VFS vfs = VFS();
 
+	public:
+		Dispatcher() = default;
+
+		void operator()(kiv_hal::TRegisters& regs);
+	};
+
+	void close_handle(kiv_hal::TRegisters& regs, VFS& vfs);
+	void create_pipe(kiv_hal::TRegisters& regs, VFS& vfs);
+	void delete_file(kiv_hal::TRegisters& regs, VFS& vfs);
+	void open_file(kiv_hal::TRegisters& regs, VFS& vfs);
+	void write_file(kiv_hal::TRegisters& regs, VFS& vfs);
+	void read_file(kiv_hal::TRegisters& regs, VFS& vfs);
+	void seek(kiv_hal::TRegisters& regs, VFS& vfs);
+	void get_file_attr(kiv_hal::TRegisters& regs, VFS& vfs);
+	void get_cwd(kiv_hal::TRegisters& regs, VFS& vfs);
+	void set_file_attr(kiv_hal::TRegisters& regs, VFS& vfs);
+	void set_cwd(kiv_hal::TRegisters& regs, VFS& vfs);
+}
