418e5a9933cd51c69599c9cade850c67aaacfcca
Štěpán Červenka <stepan.cervenka1@gmail.com>
2021-11-22 13:51:43 +0100

    Add dummy_cwd_holder

---
+++ src/kernel/filesystem/dummy_cwd_holder.cpp
@@ -0,0 +1,13 @@
+#include "dummy_cwd_holder.h"
+
+Dummy_CWD_Holder::Dummy_CWD_Holder(std::tuple<CWD, VFS_Directory2> value) {
+	Set_Value(value);
+}
+
+void Dummy_CWD_Holder::Set_Value(std::tuple<CWD, VFS_Directory2> value) {
+	this->value = value;
+}
+
+std::tuple<CWD, VFS_Directory2> Dummy_CWD_Holder::Get_Value() {
+	return this->value;
+}
---
+++ src/kernel/filesystem/dummy_cwd_holder.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "cwd.h"
+#include "vfs2/vfs_directory.h"
+
+#include <tuple>
+
+//will be replaced by process methods
+
+class Dummy_CWD_Holder {
+private:
+	std::tuple<CWD, VFS_Directory2> value;
+public:
+	Dummy_CWD_Holder(std::tuple<CWD, VFS_Directory2> value);
+	void Set_Value(std::tuple<CWD, VFS_Directory2> value);
+	std::tuple<CWD, VFS_Directory2> Get_Value();
+};
