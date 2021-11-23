732796f47255f7381cb66b09094806548c9b31e3
Štěpán Červenka <stepan.cervenka1@gmail.com>
2021-11-23 13:26:05 +0100

    Fix: cwd hold only pointers

--- src/kernel/filesystem/cwd/dummy_cwd_holder.cpp
+++ src/kernel/filesystem/cwd/dummy_cwd_holder.cpp
@@ -1,13 +1,13 @@
 #include "dummy_cwd_holder.h"
 
-Dummy_CWD_Holder::Dummy_CWD_Holder(std::tuple<CWD, VFS_Directory2> value) {
+Dummy_CWD_Holder::Dummy_CWD_Holder(std::tuple<CWD*, VFS_Directory2*> value) {
 	Set_Value(value);
 }
 
-void Dummy_CWD_Holder::Set_Value(std::tuple<CWD, VFS_Directory2> value) {
+void Dummy_CWD_Holder::Set_Value(std::tuple<CWD*, VFS_Directory2*> value) {
 	this->value = value;
 }
 
-std::tuple<CWD, VFS_Directory2> Dummy_CWD_Holder::Get_Value() {
+std::tuple<CWD*, VFS_Directory2*> Dummy_CWD_Holder::Get_Value() {
 	return this->value;
 }
--- src/kernel/filesystem/cwd/dummy_cwd_holder.h
+++ src/kernel/filesystem/cwd/dummy_cwd_holder.h
@@ -9,9 +9,9 @@
 
 class Dummy_CWD_Holder {
 private:
-	std::tuple<CWD, VFS_Directory2> value;
+	std::tuple<CWD*, VFS_Directory2*> value;
 public:
-	Dummy_CWD_Holder(std::tuple<CWD, VFS_Directory2> value);
-	void Set_Value(std::tuple<CWD, VFS_Directory2> value);
-	std::tuple<CWD, VFS_Directory2> Get_Value();
+	Dummy_CWD_Holder(std::tuple<CWD*, VFS_Directory2*> value);
+	void Set_Value(std::tuple<CWD*, VFS_Directory2*> value);
+	std::tuple<CWD*, VFS_Directory2*> Get_Value();
 };
