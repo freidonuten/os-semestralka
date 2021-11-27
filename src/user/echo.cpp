d96b8ac097d9286c774f09f0fee922ed74d6297d
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-27 09:21:32 -0800

    echo refactor

--- src/user/echo.cpp
+++ src/user/echo.cpp
@@ -3,11 +3,12 @@
 
 size_t __stdcall echo(const kiv_hal::TRegisters& regs) {
 	kiv_os::THandle stdout_handle = regs.rbx.x;
-	uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
 	char* parameters = reinterpret_cast<char *>(regs.rdi.r);
+
 	size_t chars_written = 0;
-	std::ostringstream oss(parameters);
-	kiv_os_rtl::Write_File(stdout_handle, oss.str().data(), oss.str().size(), chars_written);
+	const auto buffer = std::string_view(parameters);
+
+	kiv_os_rtl::Write_File(stdout_handle, buffer.data(), buffer.size(), chars_written);
 	kiv_os_rtl::Exit(0);
 	return 0;
 }
