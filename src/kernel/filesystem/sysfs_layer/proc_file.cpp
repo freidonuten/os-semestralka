fc07fe427f388cd5066bcd7290296394b993f1d1
Martin Procházka <martin.prochazka@protonmail.com>
2021-11-28 06:35:33 -0800

    Skip empty PCBs

--- src/kernel/filesystem/sysfs_layer/proc_file.cpp
+++ src/kernel/filesystem/sysfs_layer/proc_file.cpp
@@ -10,16 +10,22 @@ proc::File::File(const Task_Manager& tm)
 { }
 
 std::uint64_t proc::File::Read(size_t how_many_bytes, void* buffer) {
+	const auto is_running = [](const auto& i) {
+		return i.get_state() != Execution_State::FREE;
+	};
+
+	index = std::find_if_not(index, end, is_running);
+
 	if (index == end) {
 		return 0;
 	}
 
-	const auto& pcb = *index++;
+	const auto& pcb = *index;
 	auto ss = std::stringstream();
 
-	ss  << pcb.get_name() << "\t"
-		//<< pcb.get_cwd() << "\t"
-		<< pcb.get_pid() << "\n";
+	ss  //<< pcb.get_cwd() << "\t"
+		<< pcb.get_pid() << "\t"
+		<< pcb.get_name() << "\n";
 
 	const auto& result = ss.str();
 	const auto  size_limit = min(how_many_bytes, result.length());
