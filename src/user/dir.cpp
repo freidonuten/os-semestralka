39b05c48e2046ad1470c40e62dd27f8d919da8b9
Martin Lacha <martin.lacha@unicorn.com>
2021-11-30 20:46:02 +0100

    Fix dir /S command

--- src/user/dir.cpp
+++ src/user/dir.cpp
@@ -40,12 +40,15 @@ std::pair<bool, std::string> parse_args(const std::string args) {
 	auto results = std::vector<std::string>(
 		std::istream_iterator<std::string>{iss},
 		std::istream_iterator<std::string>()
-	);
+		);
 
 	if (results.size() == 1) {
+		if (results[0] == "/S") {
+			return { true, "." };
+		}
 		return { false, results[0] };
 	}
-	
+
 	if (results.size() == 2 && results[0] == "/S") {
 		return { true, results[1] };
 	}
@@ -53,9 +56,47 @@ std::pair<bool, std::string> parse_args(const std::string args) {
 	return { false, "." };
 }
 
+std::string get_output_string_path(std::string current_path, std::string file, bool isDir) {
+	char file_type = isDir == true ? '+' : ' ';
+	std::string delimiter = "\\";
+	std::string path(file_type + current_path + delimiter + file);
+	return path;
+}
+
+std::string get_path(std::string current_path, std::string file) {
+	std::string path(current_path + "\\" + file);
+	return path;
+}
+
+size_t recursive_search(std::vector<kiv_os::TDir_Entry>& entries, std::vector<std::string>& all_entries, std::string path, kiv_os::THandle stdout_handle, size_t& f_count, size_t& d_count) {
+	for (const auto entry : entries) {
+		if (utils::is_file(entry.file_attributes)) {
+			all_entries.push_back(get_output_string_path(path, entry.file_name, false));
+			continue;
+		}
+		auto current_file_handle = kiv_os::THandle();
+		auto error = kiv_os_rtl::Open_File(
+			get_path(path, entry.file_name).data(), utils::get_dir_attrs(), kiv_os::NOpen_File::fmOpen_Always, current_file_handle
+		);
+		if (error != kiv_os::NOS_Error::Success) {
+			kiv_os_rtl::Write_File(stdout_handle, utils::get_error_message(error));
+			KIV_OS_EXIT(2);
+		}
+		all_entries.push_back(get_output_string_path(path, entry.file_name, true));
+		auto sub_entries = std::vector<kiv_os::TDir_Entry>();
+		const auto [file_count, dir_count] = load_entries(current_file_handle, sub_entries);
+		f_count += file_count;
+		d_count += dir_count;
+		recursive_search(sub_entries, all_entries, get_path(path, entry.file_name), stdout_handle, f_count, d_count);
+		kiv_os_rtl::Close_Handle(current_file_handle);
+	}
+}
+
 size_t __stdcall dir(const kiv_hal::TRegisters& regs) {
 	const kiv_os::THandle stdin_handle = regs.rax.x;
 	const kiv_os::THandle stdout_handle = regs.rbx.x;
+	size_t f_count = 0;
+	size_t d_count = 0;
 
 	// parse arguments
 	auto args = std::string(reinterpret_cast<const char*>(regs.rdi.r));
@@ -66,7 +107,7 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs) {
 	auto error = kiv_os_rtl::Open_File(
 		filename.data(), utils::get_dir_attrs(), kiv_os::NOpen_File::fmOpen_Always, file_handle
 	);
-	
+
 	if (error != kiv_os::NOS_Error::Success) {
 		kiv_os_rtl::Write_File(stdout_handle, utils::get_error_message(error));
 		KIV_OS_EXIT(2);
@@ -74,18 +115,34 @@ size_t __stdcall dir(const kiv_hal::TRegisters& regs) {
 
 	// load entries
 	auto entries = std::vector<kiv_os::TDir_Entry>();
-	const auto [file_count, dir_count] = load_entries(file_handle, entries);
+	auto all_entries = std::vector<std::string>();
+	auto [file_count, dir_count] = load_entries(file_handle, entries);
+	f_count += file_count;
+	d_count += dir_count;
+
+	if (recurse) {
+		std::string path;
+		size_t chars_written = 0;
+		kiv_os_rtl::Get_Working_Dir(path.data(), BUFFER_SIZE, chars_written);
+		recursive_search(entries, all_entries, path, stdout_handle, f_count, d_count);
+	}
+	else {
+		for (const auto entry : entries) {
+			std::string file_type = utils::is_dir(entry.file_attributes) ? "+" : " ";
+			std::string entry_file(file_type + entry.file_name);
+			all_entries.push_back(entry_file);
+		}
+	}
 
 	// print
 	std::ostringstream dir_content;
 	dir_content
-		<< "Directories: " << dir_count << new_line
-		<< "Files: " << file_count << new_line;
-	
-	for (const auto entry : entries) {
+		<< "Directories: " << d_count << new_line
+		<< "Files: " << f_count << new_line;
+
+	for (const auto entry : all_entries) {
 		dir_content
-			<< (utils::is_dir(static_cast<uint8_t>(entry.file_attributes)) ? "+" : " ")
-			<< entry.file_name
+			<< entry
 			<< new_line;
 	}
 
